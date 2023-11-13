1db1d64a173783d295bd2f27cbd4ae9d7f46bae6
Utkirbek Qutfiddinov <utkirbek_qutfiddinov@mail.ru>
2023-11-13 14:37:56 +0500

    done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,25 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try (BufferedReader br=new BufferedReader(new InputStreamReader(new FileInputStream(file)))){
+            String nameLine=br.readLine();
+            String ageLine=br.readLine();
+            String emailLine=br.readLine();
+            String phoneLine=br.readLine();
+
+            return new Profile(nameLine.substring(nameLine.indexOf(" ")+1),
+                                        Integer.parseInt(ageLine.substring(ageLine.indexOf(" ")+1)),
+                                        emailLine.substring(emailLine.indexOf(" ")+1),
+                                        Long.parseLong(phoneLine.substring(phoneLine.indexOf(" ")+1)));
+
+        } catch (IOException fnfe){
+            fnfe.printStackTrace();
+        }
+        return null;
     }
 }
